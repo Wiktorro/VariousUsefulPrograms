5035c8bbae8ef9419dc49c087ca155667198f378
Wiktor Mis <mwiktor023@gmail.com>
2025-12-15 12:52:11 +0100

    add reloading aliases

--- bashrc.sh
+++ bashrc.sh
@@ -4,6 +4,29 @@
 #
 
 #
+alias bashedit='nano $HOME/.bashrc '
+alias bashreload='source $HOME/.bashrc '
+alias historyedit='nano ~/.bash_history '
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
 
 #
 
