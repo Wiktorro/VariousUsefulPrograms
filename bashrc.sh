c6576bd1d5b20e6f2a8c2cc7a9ee323204db22ad
Wiktor Mis <mwiktor023@gmail.com>
2025-12-15 13:35:24 +0100

    add visual studio terminal handling

--- bashrc.sh
+++ bashrc.sh
@@ -8,6 +8,9 @@
 #
 
 #
+# in case of error as https://stackoverflow.com/a/76741707 :: ln -s /usr/share/cmake-3.25 ~/.local/share/ 
+alias vs='"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"' \
+&& complete -o dirnames -f  -X '!*.sln' vs  # --ext=txt jak w complete -F _ext_completion --ext=txt mycmd ,  if none matches pattern, give directory names for  deep search, https://www.gnu.org/software/bash/manual/html_node/Programmable-Completion-Builtins.html#index-complete
 
 #
 
