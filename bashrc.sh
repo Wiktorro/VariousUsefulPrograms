7fd9ed8366fe06d5ea1bdd228b3ac97f3739bfd6
Wiktor Mis <mwiktor023@gmail.com>
2025-12-15 13:17:03 +0100

    add vars

--- bashrc.sh
+++ bashrc.sh
@@ -6,6 +6,8 @@
 #
 
 #
+declare EDITOR=nano ; 
+declare -rx VISUAL=${EDITOR} ED=${EDITOR} HISTFILESIZE=9999  HISTSIZE=9999 HISTCONTROL=ignoreboth:erasedups DOTNET_CLI_TELEMETRY_OUTPUT=1 2> /dev/null  
 
 #
 
