ce172eaa337707c6cb8e9797490695c7d5f80372
Wiktor Mis <mwiktor023@gmail.com>
2026-01-14 16:31:39 +0100

    add num2textConverter testing

--- bashrc.sh
+++ bashrc.sh
@@ -13,7 +13,9 @@
 
 #
 if which --all picocom &> /dev/null  ;  then
-true
+alias historyedit_picocom=" gedit ~/.picocom_history " # maybe different path
+alias picocomHKP-USB0=' echo "zamyka sie przez CTRL + A + X" && picocom -b115200 /dev/ttyUSB0' # hkp # 
+picocomHKP-USB0sendCommand() {  echo  " ${1} "  |   picocom -q -b115200 /dev/ttyUSB0 ; }
 fi
 #
 
