8c4757582e95992dc2d09606c09f671bf12cc47a
Wiktor Mis <mwiktor023@gmail.com>
2026-01-14 14:53:07 +0100

    add basic docker aliases

--- bashrc.sh
+++ bashrc.sh
@@ -15,7 +15,9 @@ declare -rx VISUAL=${EDITOR} ED=${EDITOR} HISTFILESIZE=9999  HISTSIZE=9999 HISTC
 
 #
 if which --all docker &> /dev/null ; then
-true;
+alias docker=' docker --log-level warn ' ## less output by default ## shared hist via ::
+alias dockerConfig_edit='  nano ~/.docker/config* ' ## works for ~/.docker/config as well as ~/.docker/config.json
+dockerConfig_check() { grep  "$1"  ~/.docker/config* -A 2 ; }  ## also useful " jq . -c ~/.docker/config*  " and "   jq -c '.["auths"]["registry.haleytek.net"]' ~/.docker/config* ", more at https://jqlang.github.io/jq/manual/
 fi
 
 #
