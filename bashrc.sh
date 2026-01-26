a540620353d998b7a2ee37eb7328745834f0b2d6
Wiktor Mis <mwiktor023@gmail.com>
2026-01-26 12:23:44 +0100

    include picocom completion web script

--- bashrc.sh
+++ bashrc.sh
@@ -15,6 +15,11 @@ fi
 
 #
 if which --all picocom &> /dev/null  ;  then
+if ! complete -p picocom &> /dev/null; then
+tmp=~/picocom_completion.sh
+[[ -f ${tmp} ]] || curl --fail --silent --show-error --location -k --anyauth https://raw.githubusercontent.com/npat-efault/picocom/refs/heads/master/bash_completion/picocom --output ${tmp} --write-out '%{http_code}' || echo " failed at ${LINENO} on tmp =${tmp}"  
+source ${tmp}
+fi
 true
 fi
 #
