11cddcaf5c629cfd7d1774e89a0b92eb6264e136
Wiktor Mis <mwiktor023@gmail.com>
2026-01-26 12:32:39 +0100

    Add functions for picocom serial communication

--- bashrc.sh
+++ bashrc.sh
@@ -16,6 +16,12 @@ fi
 #
 if which --all picocom &> /dev/null  ;  then
 true
+comvip() { picocom "$@" --quiet --baud 115200 /dev/ttyUSB0 ; }
+
+comvipversion() { comvip "$@"  --initstring  "$(echo -ne 'version\r\n')" ; }
+# example from https://man.archlinux.org/man/picocom.1.en :: -t "$(echo -ne 'AAATZ\r\n')" #  --omap spchex pod zmiany znakow, ale szkoda czasu
+
+comvipfastboot() {  comvip --initstring "$(echo -ne 'reset -f\r\n')" ; } # keeps terminal open and reading
 fi
 #
 
