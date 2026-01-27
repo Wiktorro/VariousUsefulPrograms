f1bdef87ad43bbf830967a3e65abde8300faf4aa
Wiktor Mis <mwiktor023@gmail.com>
2026-01-27 09:41:26 +0100

    add reloading aliases (#10)

--- bashrc.sh
+++ bashrc.sh
@@ -2,6 +2,40 @@
 #
 
 #
+
+#
+alias bashedit='nano $HOME/.bashrc '
+alias bashreload='source $HOME/.bashrc '
+alias historyedit='nano ~/.bash_history '
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
 if which --all repo &> /dev/null  ;  then
 true
 fi
