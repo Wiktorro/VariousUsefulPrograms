ea2a711111050e4f094720cf09383c0173eb1c7d
Wiktor Mis <mwiktor023@gmail.com>
2026-03-02 08:59:59 +0100

    add readFlags function (#28)

--- bashrc.sh
+++ bashrc.sh
@@ -124,7 +124,8 @@ highlight() {  ##  uname -n | highlight tek 2
 #
 
 #
-
+# adding '=*' to pattern would include '=' if provided to flag (as in --color=always), but rejected now as behavior on windows is problematic , doesnt match as intended # -- necessary fo differ from the patterns , zajete getflags przez /usr/bin/getflags, https://www.cyberciti.biz/faq/grep-regular-expressions/ # also concatenate to 1 line with paste -sd' ' 
+readFlags() { grep --only-matching --extended-regexp -- '\B-+[-a-zA-Z0-9]+' "$@" | sort --unique | tr '\n' ' ' ; }
 #
 
 #
