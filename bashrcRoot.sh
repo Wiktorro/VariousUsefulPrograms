12e7cf3257c854579d1b34d914a17dccc00da6a5
Wiktor Mis <mwiktor023@gmail.com>
2026-09-01 08:34:33 +0200

    Update bashrcRoot.sh

--- bashrcRoot.sh
+++ bashrcRoot.sh
@@ -3,7 +3,8 @@ alias fullupdate='apt-get -qqy  update '
 #
 export ME=me
 #
-alias snaplimit='snap set system refresh.timer=wed,19:00-20:00,21:00-22:00'
 if which --all fwupdmgr &> /dev/null  ;  then
 true
 fi
+
+alias snaplimit='snap set system refresh.timer=wed,19:00-20:00,21:00-22:00'
