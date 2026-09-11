375ce5cfb0e3c647454b6ca7432d1d8db45c28cf
Wiktor Mis <mwiktor023@gmail.com>
2026-09-11 09:00:03 +0200

    add journalctl placeholder (#122)

--- bashrcRoot.sh
+++ bashrcRoot.sh
@@ -10,5 +10,8 @@ fi
 if which --all fwupdmgr &> /dev/null  ;  then
 true
 fi
+if which --all journalctl &> /dev/null  ;  then
+true
+fi
 
 alias snaplimit='snap set system refresh.timer=wed,19:00-20:00,21:00-22:00'
