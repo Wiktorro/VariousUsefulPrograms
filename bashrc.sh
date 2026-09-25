f2a2816430c31af62d98b418a9f2fb9b997316de
Wiktor Mis <mwiktor023@gmail.com>
2026-09-25 10:20:56 +0200

    set TITLEPREFIX in titleSet

--- bashrc.sh
+++ bashrc.sh
@@ -106,6 +106,7 @@ alias trimSpaces="sed --regexp-extended --in-place 's-  +- -'"
 #
 alias sortinline="tr ' ' '\n' | sort | tr '\n' ' ' "
 titleSet() {
+TITLEPREFIX=$1
 echo -en "\033]0;$1\a"  ### like echo -en "\033]0;New title\a" ## better Use printf, echo isn't portable in the regard.  ,as in https://unix.stackexchange.com/q/70459
 } ##{ echo "[ $1 ]" }
 
